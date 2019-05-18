549d588b2a59fae5f5bbe0b266f70c13e45ddab8
RiceGnat <ricegnat@gmail.com>
2019-05-18 01:43:24 -0500

    solution files

---
+++ ShiftJisZipExtractor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace ShiftJisZipList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string path = args[0];
+			string output = args[1];
+
+			Extract(path, output);
+		}
+
+		private static void Extract(string path, string output)
+		{
+			// Get full paths
+			string zipPath = Path.GetFullPath(path);
+			string extractPath = Path.GetFullPath(output);
+
+			ZipFilenameReader zip = new ZipFilenameReader(zipPath);
+			IEnumerable<string> filenames = zip.GetAllFilenames();
+
+			// Ensures that the last character on the extraction path is the directory separator char
+			if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+				extractPath += Path.DirectorySeparatorChar;
+
+			using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+			{
+				IEnumerable<string> files = archive.Entries.Zip(filenames, (entry, filename) =>
+				{
+					// Get full path
+					string destinationPath = Path.GetFullPath(Path.Combine(extractPath, filename));
+
+					// Check that path is safe
+					if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+					{
+						// Create directory if it does not exist
+						Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+
+						// If it's a file, extract it
+						if (Path.GetFileName(destinationPath) != String.Empty)
+						{
+							entry.ExtractToFile(destinationPath);
+						}
+					}
+
+					return destinationPath;
+				});
+				foreach (string file in files)
+				{
+					Console.WriteLine(file);
+				}
+			}
+		}
+	}
+}
---
+++ ShiftJisZipExtractor/ZipFilenameReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftJisZipList
+{
+	public class ZipFilenameReader
+	{
+		
+		private const int FILE_HEADER_SIGNATURE = 0x04034b50; // Reversed for endianness
+		private const int FILE_NAME_LEN_OFFSET = 0x001a;
+		private const int EXTRA_FIELD_LEN_OFFSET = 0x001c;
+		private const int FILE_NAME_OFFSET = 0x001e;
+		private const int COMPRESSED_SIZE_OFFSET = 0x0012;
+
+		private class ZipEntry
+		{
+			private byte[] bytes;
+			private int offset;
+			private int filenameLen;
+
+			public ZipEntry(byte[] bytes, int offset)
+			{
+				this.bytes = bytes;
+				this.offset = offset;
+				filenameLen = BitConverter.ToInt16(bytes, offset + FILE_NAME_LEN_OFFSET);
+			}
+
+			public string GetFilename()
+				=> Encoding.GetEncoding("shift_jis").GetString(bytes, offset + FILE_NAME_OFFSET, filenameLen);
+
+			public int GetBlockSize()
+			{
+				// Ignoring data descriptor and ZIP64 for now until it becomes a problem
+
+				int extraLen = BitConverter.ToInt16(bytes, offset + EXTRA_FIELD_LEN_OFFSET);
+				int compressedSize = BitConverter.ToInt32(bytes, offset + COMPRESSED_SIZE_OFFSET);
+
+				return FILE_NAME_OFFSET + extraLen + compressedSize;
+			}
+		}
+
+		private byte[] bytes;
+
+		public ZipFilenameReader(string path)
+		{
+			bytes = File.ReadAllBytes(path);
+		}
+		
+		public IEnumerable<string> GetAllFilenames()
+		{
+			List<string> filenames = new List<string>();
+
+			int i = 0;
+			while (i < bytes.Length - FILE_NAME_OFFSET)
+			{
+				// See if we're at a file signature
+				if (BitConverter.ToInt32(bytes, i) == FILE_HEADER_SIGNATURE)
+				{
+					ZipEntry entry = new ZipEntry(bytes, i);
+					filenames.Add(entry.GetFilename());
+					i += entry.GetBlockSize();
+				}
+				else // Keep looking
+				{
+					i++;
+				}
+			}
+
+			return filenames;
+		}
+	}
+}
